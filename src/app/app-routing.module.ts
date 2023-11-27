4ee5479a616025738c865cec747bc517d5619311
ZaikShaiz <zaikdmr@universalplus.technology>
2023-11-27 16:34:51 +0400

    icons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './home/home.component';
+import { MarketDetailsComponent } from './market-details/market-details.component';
 
 const routes: Routes = [
   { path: 'layout', component: LayoutComponent },
@@ -9,6 +10,7 @@ const routes: Routes = [
     path: '', component: LayoutComponent,
     children: [
       { path: '', component: HomeComponent },
+      { path: 'market', component: MarketDetailsComponent },
     ]
   }
 ];

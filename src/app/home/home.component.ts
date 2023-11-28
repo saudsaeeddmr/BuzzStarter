77bad4276fe1b6c6c37e2e9802d362e6192d5060
ZaikShaiz <zaikdmr@universalplus.technology>
2023-11-28 15:13:35 +0400

    home inplay market

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InplayComponent } from "../inplay/inplay.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css'],
+    imports: [CommonModule, InplayComponent]
 })
 export class HomeComponent {
 
---
+++ src/app/inplay/inplay.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-inplay',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './inplay.component.html',
+  styleUrls: ['./inplay.component.css']
+})
+export class InplayComponent {
+
+}

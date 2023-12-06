d3d4b2bc1a681116231c9af4de66eaea134f7eee
ZaikShaiz <zaikdmr@universalplus.technology>
2023-12-06 18:50:23 +0400

    side nav destop

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,6 +8,42 @@ import { CommonModule } from '@angular/common';
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.css']
 })
-export class SideNavComponent {
+export class SideNavComponent implements OnInit{
+
+tabOpen: boolean = false;
+subTab: boolean = false;
+
+marketData = [
+      { sportname : "criket1" },
+      { sportname : "criket2" },
+      { sportname : "criket3" },
+      { sportname : "criket4" },
+      { sportname : "criket5" },
+      { sportname : "criket6" },
+      { sportname : "criket7" },
+      { sportname : "criket8" },
+      
+];
+  
+ngOnInit(): void {
+  console.log('asdfghjhgfd',this.marketData);
+}
+goToNxtTab() {
+this.tabOpen = true; 
+}
+goToPrivous(){
+  // console.log('========',this.subTab);
+  this.tabOpen = false;
+}
+goToPrivousSub(){
+  // console.log('========',this.subTab);
+  // this.tabOpen = false;
+  this.subTab = false;
+
+}
+goToSubTab(){
+  // this.tabOpen = false;
+  this.subTab = true
+}
 
 }

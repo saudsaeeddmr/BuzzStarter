69800fd30484be5150ae2cac228c3960931e463a
ZaikShaiz <zaikdmr@universalplus.technology>
2023-12-07 11:15:39 +0400

    changes

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -8,42 +8,45 @@ import { CommonModule } from '@angular/common';
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.css']
 })
-export class SideNavComponent implements OnInit{
+export class SideNavComponent implements OnInit {
 
-tabOpen: boolean = false;
-subTab: boolean = false;
+  tabOpen: boolean = false;
+  subTab: boolean = false;
 
-marketData = [
-      { sportname : "criket1" },
-      { sportname : "criket2" },
-      { sportname : "criket3" },
-      { sportname : "criket4" },
-      { sportname : "criket5" },
-      { sportname : "criket6" },
-      { sportname : "criket7" },
-      { sportname : "criket8" },
-      
-];
-  
-ngOnInit(): void {
-  console.log('asdfghjhgfd',this.marketData);
-}
-goToNxtTab() {
-this.tabOpen = true; 
-}
-goToPrivous(){
-  // console.log('========',this.subTab);
-  this.tabOpen = false;
-}
-goToPrivousSub(){
-  // console.log('========',this.subTab);
-  // this.tabOpen = false;
-  this.subTab = false;
+  marketData = [
+    { sportname: "criket1" },
+    { sportname: "criket2" },
+    { sportname: "criket3" },
+    { sportname: "criket4" },
+    { sportname: "criket5" },
+    { sportname: "criket6" },
+    { sportname: "criket7" },
+    { sportname: "criket8" },
 
-}
-goToSubTab(){
-  // this.tabOpen = false;
-  this.subTab = true
-}
+  ];
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log('asdfghjhgfd', this.marketData);
+  }
+  goToNxtTab() {
+    this.tabOpen = true;
+  }
+  goToPrivous() {
+    // console.log('========',this.subTab);
+    this.tabOpen = false;
+  }
+  goToPrivousSub() {
+    // console.log('========',this.subTab);
+    // this.tabOpen = false;
+    this.subTab = false;
+
+  }
+  goToSubTab() {
+    // this.tabOpen = false;
+    this.subTab = true
+  }
 
 }

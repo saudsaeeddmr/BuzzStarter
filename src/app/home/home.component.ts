e92347c9d8d16ee20a6c56b35d362c115492204a
ZaikShaiz <zaikdmr@universalplus.technology>
2023-12-08 18:14:20 +0400

    top carusal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InplayComponent } from "../inplay/inplay.component";
 import { RightblockComponent } from "../rightblock/rightblock.component";
+import { TopcarusalComponent } from "../topcarusal/topcarusal.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css'],
-    imports: [CommonModule, InplayComponent, RightblockComponent]
+    imports: [CommonModule, InplayComponent, RightblockComponent, TopcarusalComponent]
 })
 export class HomeComponent {
 
---
+++ src/app/topcarusal/topcarusal.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-topcarusal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './topcarusal.component.html',
+  styleUrls: ['./topcarusal.component.css']
+})
+export class TopcarusalComponent {
+
+}
